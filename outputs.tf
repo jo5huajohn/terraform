d1a4a408bf069a96facc6b580f1ad5cf79151250
Joshua John <joshuajohnv15@gmail.com>
2025-08-21 00:32:03 -0500

    Removed container IP output due to lack of correct output
    Signed-off-by: Joshua John <joshuajohnv15@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,3 @@ output "mealie_ipv4_address" {
   value = proxmox_virtual_environment_vm.mealie_vm.ipv4_addresses[1][0]
 }
 
-output "nextcloud_ipv4_address" {
-  value = proxmox_virtual_environment_container.nextcloud_container.ipv4.veth0
-}
-
-output "paperless_ngx_ipv4_address" {
-  value = proxmox_virtual_environment_container.paperless_ngx_container.ipv4.veth0
-}
