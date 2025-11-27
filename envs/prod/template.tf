fdbc423245e6da66323cf770923b12598fc9a536
Joshua John <joshuajohnv15@gmail.com>
2025-11-26 21:39:25 -0600

    Add Fedora 43 cloud image template
    Signed-off-by: Joshua John <joshuajohnv15@gmail.com>

--- envs/prod/template.tf
+++ envs/prod/template.tf
@@ -6,6 +6,73 @@ data "local_file" "ssh_pub_key" {
   filename = pathexpand("~/.ssh/id_ed25519.pub")
 }
 
+resource "proxmox_virtual_environment_vm" "fedora_43_cloud_image_template" {
+  name      = "fedora-43-template"
+  node_name = "pve01"
+
+  template        = true
+  started         = false
+  stop_on_destroy = true
+  on_boot         = true
+
+  bios          = "ovmf"
+  machine       = "q35"
+  scsi_hardware = "virtio-scsi-single"
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 1
+    type  = "x86-64-v2-AES"
+  }
+
+  disk {
+    datastore_id = "vms"
+    import_from  = proxmox_virtual_environment_download_file.fedora_cloud_43_cloud_image.id
+    interface    = "scsi0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+    ssd          = true
+  }
+
+  efi_disk {
+    datastore_id      = "vms"
+    type              = "4m"
+    pre_enrolled_keys = true
+  }
+
+  initialization {
+    datastore_id = "vms"
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+      ipv6 {
+        address = "auto"
+      }
+    }
+
+    user_account {
+      keys     = [
+        trimspace(data.local_file.ssh_pub_key.content)
+      ]
+      password = "4452218"
+      username = "admin"
+    }
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  operating_system {
+    type = "l26"
+  }
+}
+
 resource "proxmox_virtual_environment_vm" "ubuntu_noble_cloud_image_template" {
   name      = "ubuntu-noble-template"
   node_name = "pve01"
