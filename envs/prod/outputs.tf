4213379f5ead1f61b6750c152f8c5d30b6c99b7a
Joshua John <joshuajohnv15@gmail.com>
2025-11-27 05:17:55 +0000

    Add netboot VM IP output
    Signed-off-by: Joshua John <joshuajohnv15@gmail.com>

--- envs/prod/outputs.tf
+++ envs/prod/outputs.tf
@@ -5,3 +5,7 @@ output "immich_ipv4_address" {
 output "mealie_ipv4_address" {
   value = proxmox_virtual_environment_vm.mealie_vm.ipv4_addresses[1][0]
 }
+
+output "netboot_ipv4_address" {
+  value = proxmox_virtual_environment_vm.netboot_vm.ipv4_addresses[1][0]
+}
