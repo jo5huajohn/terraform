0d44aad33008d7e2d7faa8783d431968359405e7
Joshua John <joshuajohnv15@gmail.com>
2026-01-01 16:29:52 +0000

    Move username and password to tfvars
    Signed-off-by: Joshua John <joshuajohnv15@gmail.com>

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -7,6 +7,11 @@ variable "virtual_environment_api_token" {
 variable "virtual_environment_vm_username" {
 }
 
+variable "virtual_environment_user_account_password" {
+    type        = string
+    description = "Password of user in virtual environment"    
+}
+
 variable "ssh_pub_key" {
     type        = string
     description = "SSH public key for users"
