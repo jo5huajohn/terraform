846a73b34435663d2785c2ff3d0085188d8f458d
Joshua John <joshuajohnv15@gmail.com>
2026-01-18 03:34:26 +0000

    Fix indents in variables.tf
    Signed-off-by: Joshua John <joshuajohnv15@gmail.com>

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,22 +1,25 @@
 variable "virtual_environment_address"  {
-    description = "Address of the node"
-    type        = string
+  description = "Address of the node"
+  type        = string
 }
 
 variable "virtual_environment_api_token" {
+  type        = string
+  sensitive   = true
 }
 
 variable "virtual_environment_vm_username" {
+  type        = string
+  sensitive   = true
 }
-
 variable "virtual_environment_user_account_password" {
-    description = "Password of user in virtual environment"    
-    type        = string
-    sensitive   = true
+  description = "Password of user in virtual environment"
+  type        = string
+  sensitive   = true
 }
 
 variable "ssh_pub_key" {
-    description = "SSH public key for users"
-    type        = string
-    sensitive   = true
+  description = "SSH public key for users"
+  type        = string
+  sensitive   = true
 }
